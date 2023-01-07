550930219ad55f8975bb84946a022c4763726f66
amenesesc <amenesesc@indracompany.com>
2023-01-07 13:01:29 -0500

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/registerUser/RegisterUser";
+import Dashboard from "./pages/dashboard/Dashboard";
+import { ListItemsProvider } from "./context/ListItemContext";
+import { UseList } from './hooks/useList'
 
 function App() {
+  const { listItems, listOfPokemons, getListItems } = UseList();
+
   return (
-    <div className="App">
-      holi
-    </div>
+    <ListItemsProvider value={{ listItems, listOfPokemons, getListItems }}>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </ListItemsProvider>
   );
 }
 
