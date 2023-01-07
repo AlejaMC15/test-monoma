1e6d7c830a1009859fe8a862df8e7add749fe137
amenesesc <amenesesc@indracompany.com>
2023-01-07 13:01:10 -0500

    Dashboard page

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,98 @@
+import React, { useContext } from "react";
+import {
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+  Avatar,
+  Pagination
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import ListItemContext from "../../context/ListItemContext";
+
+const Dashboard = () => {
+
+  const { listItems, listOfPokemons, getListItems } = useContext(ListItemContext);
+
+  console.log(listItems, listOfPokemons, getListItems);
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <img style={{ width: "10%" }} src={require('../../assets/images/logo.png')} alt="" />
+          <Typography variant="h4" color="inherit" noWrap>
+            MONOMA
+          </Typography>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://source.unsplash.com/random"
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+              </CardActions>
+            </Card>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Pagination count={10} />
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+      </Box>
+      {/* End footer */}
+    </React.Fragment>
+  );
+};
+
+export default Dashboard;
