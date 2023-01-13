18b97a8d1859362876b2045e4d4ccba244e883f6
amenesesc <amenesesc@indracompany.com>
2023-01-12 19:24:07 -0500

    Add new styles

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -23,19 +23,28 @@ import Modal from "@mui/material/Modal";
 import { useNavigate } from "react-router-dom";
 
 import './Dashboard.scss'
-import { it } from "node:test";
 
 const Dashboard = () => {
+
+  interface ItemSelectedData {
+    sprites: any,
+    other: any,
+    dream_world: any,
+    front_default: any,
+    name: any,
+    abilities: any,
+    ability: any
+  }
 
   const { listItemsp, listOfPokemons, GetListItems, GetListUrl, load } = useContext(ListItemContext);
 
   const navigate = useNavigate();
 
   const [open, setOpen] = useState(false);
-  const [itemSelected, setItemSelected] = useState([]);
+  const [itemSelected, setItemSelected] = useState<ItemSelectedData[]>([]);
 
-  const handleOpen = (id: any[]) => {
-    const itemSelect = listOfPokemons.filter((item: any) => item.id === id)
+  const handleOpen = async (id: any[]) => {
+    const itemSelect: any = await listOfPokemons.filter((item: any) => item.id === id)
     setItemSelected(itemSelect)
     setOpen(true)
   };
@@ -44,9 +53,6 @@ const Dashboard = () => {
     setOpen(false)
   };
 
-  console.log(itemSelected);
-
-
   useEffect(() => {
     GetListItems()
   }, [])
@@ -54,6 +60,23 @@ const Dashboard = () => {
   useEffect(() => {
     listItemsp !== undefined && GetListUrl(listItemsp)
   }, [listItemsp])
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const openMenu = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const nextPage = async () => {
+   await GetListItems(listItemsp.data.next);
+  }
+  const previusPage = () => {
+    GetListItems(listItemsp.previous);
+  }
 
   const style = {
     position: 'absolute' as 'absolute',
@@ -65,16 +88,7 @@ const Dashboard = () => {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-  };
-
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const openMenu = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleCloseMenu = () => {
-    setAnchorEl(null);
-  };
+  };  
 
   return (
     <React.Fragment>
@@ -117,24 +131,25 @@ const Dashboard = () => {
                       pt: 8,
                       pb: 6,
                     }}
-
                   >
                     <Container maxWidth="sm">
-                      <Card sx={{ maxWidth: 345 }} onClick={() => handleOpen(item.id)}>
+                      <Card className="card" onClick={() => handleOpen(item.id)}>
                         <CardMedia
                           component="img"
                           height="194"
                           image={item.sprites.other.dream_world.front_default}
                           alt="Paella dish"
                         />
-                        <Grid className='ContainerbuttonWeight'>
-                          <Typography className='buttonWeight' variant="body2" color="text.secondary">{item.weight}</Typography>
+                        <Grid sx={{ display: 'flex', justifyContent: "right" }}>
+                          <Grid className='containerbuttonWeight'>
+                            <Typography className='buttonWeight' variant="body2" color="text.secondary">{item.weight}</Typography>
+                          </Grid>
                         </Grid>
                         <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
+                          <Typography className="nameItem" gutterBottom variant="h4" component="div">
                             {item.name}
                           </Typography>
-                          <Typography variant="body2" color="text.secondary">
+                          <Typography className="nameItem" variant="body2" color="text.secondary" sx={{ color: 'darkgreen' }}>
                             {item.moves.slice(0, 2).map((move: any) => {
                               return move.move.name.replace('-', ' ')
                             })}
@@ -156,16 +171,13 @@ const Dashboard = () => {
 
           <Grid container justifyContent="center">
             <Grid item>
-              <Pagination count={10} />
+              <Pagination count={10} onChange={nextPage} />
             </Grid>
           </Grid>
         </Container>
       </main>
 
       <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
         <Typography
           variant="subtitle1"
           align="center"
@@ -183,12 +195,12 @@ const Dashboard = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style} >
-          <img src={itemSelected !== undefined && itemSelected[0]?.sprites?.other?.dream_world.front_default} alt="pokemon" />
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {itemSelected?.name}
+          <img src={itemSelected.length > 0 && itemSelected[0]?.sprites?.other?.dream_world.front_default} alt="pokemon" />
+          <Typography className="nameItem" id="modal-modal-title" variant="h3" component="h2">
+            {itemSelected[0]?.name}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Habilities: {itemSelected?.abilities?.map((itm: any) => { return ability.name })}
+            Habilities: {itemSelected[0]?.abilities?.map((itm: any) => { return itm.ability.name.replace('-', ' ') })}
           </Typography>
         </Box>
       </Modal>
